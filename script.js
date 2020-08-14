dd86a522d0368a191b9b14c75daeb9df0a1dd00e
Mandy Hom <amanda@theshoppad.com>
2020-08-14 13:19:21 -0700

    updates part 2, stuck on adding and removing book

--- script.js
+++ script.js
@@ -1,93 +1,157 @@
-(function() {
-  let myLibrary = [];
+;(function() {
+  let myLibrary = []
+  console.log(myLibrary)
 
   // Function Objects - Constructor
-  function book(title, author, read, rating) {
-    this.title = title;
-    this.author = author;
-    this.read = read;
-    this.rating = rating;
-  }
-
-  // Function to display form
-  function displayForm() {
-    let modal = document.getElementById("modal");
-    if (modal.style.display === "none") {
-      modal.style.display = "block";
-    } else {
-      modal.style.display = "none";
-    }
+  function Book(title, author, read, rating) {
+    this.title = title
+    this.author = author
+    this.read = read
+    this.rating = rating
   }
 
   // Function to add book to array
   function addBook(title, author, read, rating) {
-    let newBook = new book(title, author, read, rating);
-    myLibrary.push(newBook);
-    console.log(myLibrary);
+    let book = new Book(title, author, read, rating)
+    myLibrary.push(book)
   }
 
   // Render Function
   function render() {
-    let libraryLength = myLibrary.length;
-    for (let i = 0; i < libraryLength; i++) {}
-    return;
+    myLibrary.forEach(function(book, index) {
+      const newBook = document.createElement('div')
+      newBook.setAttribute('class', 'book')
+      newBook.setAttribute('id', index)
+
+      const title = document.createElement('div')
+      title.setAttribute('class', 'bookTitle')
+      title.textContent = 'Title: ' + book.title
+
+      const author = document.createElement('div')
+      author.setAttribute('class', 'description')
+      author.setAttribute('id', 'bookAuthor')
+      author.textContent = 'Author: ' + book.author
+
+      const read = document.createElement('div')
+      read.setAttribute('class', 'description')
+      read.setAttribute('id', 'bookRead')
+      read.textContent = book.read
+
+      const rating = document.createElement('div')
+      rating.setAttribute('class', 'description')
+      rating.setAttribute('id', 'bookRating ')
+      rating.textContent = 'Rating: ' + book.rating
+
+      const mainCard = document.createElement('div')
+      mainCard.setAttribute('class', 'card-main')
+
+      newBook.appendChild(title)
+      mainCard.appendChild(author)
+      mainCard.appendChild(read)
+      mainCard.appendChild(rating)
+      newBook.appendChild(mainCard)
+
+      let currentDiv = document.getElementById('card-content')
+      currentDiv.appendChild(newBook)
+
+      let bookIcon = document.createElement('div')
+      bookIcon.setAttribute('class', 'material-icons')
+      bookIcon.setAttribute('id', 'book')
+      bookIcon.textContent = 'book'
+      mainCard.appendChild(bookIcon)
+
+      let removeButton = document.createElement('div')
+      removeButton.setAttribute('class', 'material-icons')
+      removeButton.setAttribute('id', 'delete')
+      removeButton.textContent = 'delete'
+      mainCard.appendChild(removeButton)
+    })
   }
 
-  // Number Validation for Form
-  function numValidation() {
-    let numResponse = document.getElementById("text-rating").value;
-    let text;
-    let modal = document.getElementById("modal");
-    if (isNaN(numResponse)) {
-      text = "Input not valid";
-      console.log("error");
-      modal.style.display = "block";
-    } else if (numResponse == "") {
-      text = "";
-    }
-    document.getElementById("numberValidation").innerHTML = text;
-    // addBook()
-  }
   // Function to delete book from array
   function removeBook() {
-    console.log("remove");
+    bookDelete = document.getElementById('delete')
   }
+
+  // Function to display form
+  function displayForm() {
+    let modal = document.getElementById('modal')
+    if (modal.style.display === 'none') {
+      modal.style.display = 'block'
+    } else {
+      modal.style.display = 'none'
+    }
+  }
+
+  // Validation for Form
+  function validation() {
+    let text
+    let modal = document.getElementById('modal')
+    let titleResponse = document.getElementById('text-title').value
+    let authorResponse = document.getElementById('text-author').value
+    let readResponse = document.getElementById('text-read').value
+    let numResponse = document.getElementById('text-rating').value
+    if (titleResponse == '' || authorResponse == '' || readResponse == '' || numResponse == '') {
+      text = 'Field is required.'
+      console.log('required error')
+      modal.style.display = 'block'
+      document.getElementById('requiredValidation').innerHTML = text
+    } else if (isNaN(numResponse)) {
+      text = 'Input is not valid. Please input a number between 0 and 10. '
+      console.log('num error')
+      modal.style.display = 'block'
+      document.getElementById('numberValidation').innerHTML = text
+    } else if (numResponse < 0 || numResponse > 10) {
+      text = 'Input is not between 0 and 10. Please try again.'
+      console.log('integer error')
+      document.getElementById('inputValidation').innerHTML = text
+    } else {
+      submitBook()
+      console.log('submitting')
+    }
+  }
+
+  // Submit Book
+  function submitBook() {
+    let title = document.getElementById('text-title').value
+    let author = document.getElementById('text-author').value
+    let read = document.getElementById('text-read').checked
+    let rating = document.getElementById('text-rating').value
+    addBook(title, author, read, rating)
+    render()
+  }
+
+  // Toggle
 
   // Hide form until clicked
   function hideForm() {
-    let modal = document.getElementById("modal");
-    modal.style.display = "none";
+    let modal = document.getElementById('modal')
+    modal.style.display = 'none'
   }
 
   // Load default books
   function defaultBooks() {
-    let book1 = new book(
-      "Harry Potter and the Goblet of Fire",
-      "J. K. Rowling",
-      "read",
-      "10/10"
-    );
-    let book2 = new book("Book2", "author", "read", "4/10");
-    myLibrary.push(book1, book2);
+    let book1 = new Book('Harry Potter and the Goblet of Fire', 'J. K. Rowling', 'read', '10/10')
+    let book2 = new Book('East of Eden', 'John Steinbeck', 'Read', '8')
+    myLibrary.push(book1, book2)
   }
 
   // Document's Main Function
   function main() {
-    let newBook = document.getElementById("newBook");
-    newBook.addEventListener("click", displayForm);
+    let newBook = document.getElementById('newBook')
+    newBook.addEventListener('click', displayForm)
 
-    let deleteBook = document.getElementById("delete");
-    deleteBook.addEventListener("click", removeBook);
+    let bookSubmit = document.getElementById('submit')
+    bookSubmit.addEventListener('click', validation)
 
-    let bookSubmit = document.getElementById("submit");
-    bookSubmit.addEventListener("click", numValidation);
+    let deleteBook = document.getElementById('delete')
+    // deleteBook.addEventListener("click", removeBook)
 
-    defaultBooks();
-    render();
-    hideForm();
-    numValidation();
+    defaultBooks()
+    render()
+    hideForm()
   }
-  document.addEventListener("DOMContentLoaded", function() {
-    main();
-  });
-})();
+  document.addEventListener('DOMContentLoaded', function() {
+    main()
+  })
+})()
